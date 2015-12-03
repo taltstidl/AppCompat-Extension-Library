0195b68c6dbdbba3215d3e640e2603b2dfa9f038
TR4Android <t.r.altstidl@freenet.de>
2015-12-03 19:47:57 +0100

    Refactor ThemeUtils
    Pull Request #10

--- appcompat-extension/src/main/java/com/tr4android/support/extension/utils/ThemeUtils.java
+++ appcompat-extension/src/main/java/com/tr4android/support/extension/utils/ThemeUtils.java
@@ -3,13 +3,10 @@ package com.tr4android.support.extension.utils;
 import android.content.Context;
 import android.content.res.TypedArray;
 
-/**
- * Created by Athos on 01/12/2015.
- */
 public class ThemeUtils {
+    private static final int[] TEMP_ARRAY = new int[1];
 
     public static int getThemeAttrColor(Context context, int attr) {
-        int[] TEMP_ARRAY = new int[1];
         TEMP_ARRAY[0] = attr;
         TypedArray a = context.obtainStyledAttributes(null, TEMP_ARRAY);
         try {
