453ceb5f4a6e26b932ac322cc000aafa8812848a
TR4Android <t.r.altstidl@freenet.de>
2015-12-21 18:42:20 +0100

    Update copyright notices

--- appcompat-extension/src/main/java/com/tr4android/support/extension/internal/AccountAdapter.java
+++ appcompat-extension/src/main/java/com/tr4android/support/extension/internal/AccountAdapter.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright (C) 2015 Thomas Robert Altstidl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.tr4android.support.extension.internal;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.tr4android.appcompat.extension.R;
@@ -14,7 +28,6 @@ import com.tr4android.support.extension.widget.CircleImageView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Adapter for accounts
