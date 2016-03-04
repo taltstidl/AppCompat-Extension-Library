636cc2d464ca897d3913ee9ba532a317336b61a9
TR4Android <t.r.altstidl@freenet.de>
2016-03-04 16:24:07 +0100

    Improve sample

--- app/src/main/java/com/tr4android/appcompatextension/activity/SampleActivity.java
+++ app/src/main/java/com/tr4android/appcompatextension/activity/SampleActivity.java
@@ -1,22 +1,15 @@
 package com.tr4android.appcompatextension.activity;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -130,13 +123,7 @@ public class SampleActivity extends BaseActivity {
         });
 
         // Setup DatePickerDialog and TimePickerDialog
-        findViewById(R.id.fab_datepicker).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchPickerDialogs();
-            }
-        });
-        findViewById(R.id.fab_timepicker).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.pickers_detail).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchPickerDialogs();
